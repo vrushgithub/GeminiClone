32c235936adbcd41cad401969ec797e37d84394a
vrushgithub <cvrushabh01@gmail.com>
2024-10-26 03:19:10 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar  from './components/Sidebar'
+import Main from './components/Main';
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  );
+    
+};
+export default App;
